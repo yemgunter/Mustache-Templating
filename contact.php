59173c15dc15f11b48ed562cd865ff85ddca1385
examplename <yolanda.mcrae@g.austincc.edu>
2019-10-08 03:57:18 -0500

    Updated contact html/php
    Previous versions failed to load. Removed extra coding from 04. Set div body back. Need to set css straight for form field inputs.

--- contact.php
+++ contact.php
@@ -31,63 +31,10 @@ $header_data = ["pagetitle" => "Contact Page"];
 //this is empty because there is no data to send to the body in this example
 $body_data = [];
 
-function redirect($url) {
-    ob_start();
-    header('Location: ' . $url);
-    ob_end_flush();
-    die();
-}
-
-function main() {
-    //this first if statement tests to make sure we have a valid $_POST array
-    if (!empty($_POST)) {
- 
-        /* Cleaning routines below will strip anything harmful or extraneous out 
-         * out of the submitted $_POST variables. */
-        $name = substr(strip_tags(trim($_POST['name'])),0,64);
-        $subject = substr(strip_tags(trim($_POST['subject'])),0,64);
-        $message = substr(strip_tags(trim($_POST['message'])),0,64);
-        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? $_POST['email'] : $email = "";
-        
-        /* Test cleaned variable here. If we find and empty variable , we stop
-         * processing because that means someone tried to send us something malicious or 
-         * or incorrect.*/
-        if (!empty($name) && !empty($email) && !empty($subject) && !empty($message)) {
-            
-            /* This forms the corect email headers to send an email */
-            $headers = "FROM: $email\r\n";
-            $headers .= "Reply-To: $email\r\n";
-            $headers .= "MIME-Version: 1.0\r\n";
-            $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-            
-            
-            /* Now try to send the email. If it succeeds, we redirect to a 
-             * success page. if not, we redirect to an error page. */
-            if (mail('yolanda@yolandagunter.com', $subject, $name . '\n\n' . $message, $headers)) {
-                redirect('templates/success.html');
-            } else {
-                /* The query string at the end of each error redirect is so we 
-                 * can tell which error triggered. In this case it has no effect
-                 * on the redirect because we ignore it up on redirect. In a real
-                 * production application we can capture the query string and 
-                 * tailor our error message to the type of error. */
-                redirect('templates/error.html?error=1');
-            }
-        } else {
-            redirect('templates/error.html?error=2');
-        } 
-    } else {
-            redirect('templates/error.html?error=3');
-        }
-}
-
-//this calls main - kicks off the script
-main();
-
 //this is being used to send a footer title and local time to the footer
 $footer_data = [
     "localtime" => date('l jS \of F Y h:i:s A'),
-    "footertitle" => "Success Page"];
+    "footertitle" => "Contact Page"];
 
 /*
  * this combines the variables with the templates and creates a complete web page.
